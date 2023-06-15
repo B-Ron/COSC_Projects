6686ff6d6201e2970235a2c3c507ee5e7f4109f7
ByronW <ronwilson402@gmail.com>
2023-06-14 22:45:44 -0400

    Started Game window and implement background

--- 2d Fighter/main.py
+++ 2d Fighter/main.py
@@ -9,6 +9,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Brawler")
 
+#load background image
+bg_image = pygame.image.load("assets/images/background/backgound.jpg").convert_alpha()
+
+
 #game loop
 run = True
 while run:
