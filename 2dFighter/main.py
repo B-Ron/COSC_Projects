8bf3cdc18be3f3ef79f786c9306b91676284f2ce
ByronW <ronwilson402@gmail.com>
2023-07-06 23:13:58 -0400

    Fixed Background and Created temp Fighters

--- 2dFighter/main.py
+++ 2dFighter/main.py
@@ -1,4 +1,5 @@
 import pygame, os
+from fighter import Fighter
 
 pygame.init()
 
@@ -12,16 +13,34 @@ pygame.display.set_caption("Brawler")
 #load background image
 bg_image = pygame.image.load(r"C:\Users\ronwi\.vscode\COSC_Projects\2dFighter\assets\images\background\background.jpg").convert()
 
+#function for drawing background
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image,(SCREEN_WIDTH,SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0,0))
+
+#create two instances for fighters
+
+fighter1 = Fighter(200,310)
+fighter2 = Fighter(700,310)
+
 #game loop
 run = True
 while run:
     
+    #draw background
+    draw_bg()
     
+    #draw fighters
+    fighter1.draw(screen)
+    fighter2.draw(screen)
     
     #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    
+    #update display
+    pygame.display.update()
             
 #quit game
 pygame.quit()
