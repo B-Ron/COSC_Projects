bc18f3db044df0ff64759aaf2a4d9d99852c0e91
ByronW <ronwilson402@gmail.com>
2023-09-06 13:25:07 -0400

    Combined website folders/files

---
+++ WebsiteProj/website_main.py
@@ -0,0 +1,7 @@
+def create_app():
+
+    app = create_app() #create app
+
+    if __name__ == "__main__":
+        app.run(debug=True) # only run web server through this file
+    
--- website_main.py
+++
@@ -1,7 +0,0 @@
-from WebsiteProj import create_app
-
-app = create_app() #create app
-
-if __name__ == "__main__":
-    app.run(debug=True) # only run web server through this file
-    
